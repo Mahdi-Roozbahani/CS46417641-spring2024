110424d3e658e3a705cbee54bb665c6dc02b13d4
mahdir <mahdir@gatech.edu>
2023-01-04 12:01:14 -0500

    Publishing site build - Wed Jan  4 12:01:14 EST 2023

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-# echo "\nClearing old docs..."
-# rm -rf ./docs
-# echo "\nBuilding new site with hugo...\n"
-# hugo
-# echo "\nDeploying site to Github...\n"
-# mv ./public ./docs
-# sleep 5
+echo "\nClearing old docs..."
+rm -rf ./docs
+echo "\nBuilding new site with hugo...\n"
+hugo
+echo "\nDeploying site to Github...\n"
+mv ./public ./docs
+sleep 5
 git add --all
 git commit -a -m "Publishing site build - $(date)"
 git push -u origin main --force
